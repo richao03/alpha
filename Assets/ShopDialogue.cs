bd6f61e41eceafcff6c995b7fd50181c2621c30e
Richard Chao <r.chao03@gmail.com>
2019-03-04 09:42:25 -0500

    start working on shop

---
+++ Assets/ShopDialogue.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShopDialogue : MonoBehaviour
+{
+  [SerializeField] TextMeshProUGUI textBox;
+  [TextArea(15, 20)] [SerializeField] List<string> TextList;
+  [SerializeField] string talkingText = "";
+  private string currentText;
+  // Start is called before the first frame update
+  void Start()
+  {
+    StartCoroutine("StartTalking");
+  }
+
+  private void CheckDialogue()
+  {
+    if (Input.GetButtonDown("Fire1"))
+    {
+      print("fire");
+      StopCoroutine("StartTalking");
+      talkingText = currentText;
+    }
+
+  }
+  private IEnumerator StartTalking()
+  {
+
+    for (int i = 0; i < TextList.Count; i++)
+    {
+      talkingText = "";
+      currentText = TextList[i];
+      if (talkingText.Length != currentText.Length)
+      {
+        for (int j = 0; j < TextList[i].Length; j++)
+        {
+          talkingText = talkingText + TextList[i][j];
+          yield return new WaitForSeconds(.04f);
+        }
+      }
+      else
+      {
+        print("TEXT DONE");
+        yield return new WaitForSeconds(2f);
+
+      }
+    }
+  }
+  // Update is called once per frame
+  void Update()
+  {
+    textBox.text = talkingText;
+    CheckDialogue();
+  }
+}
